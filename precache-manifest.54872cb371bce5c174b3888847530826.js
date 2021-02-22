633fd5107e7a405642c81d83df68bbd3d6718fd9
Olena Kaliuzhna <om.kaliuzhna.dz@gmail.com>
2021-02-22 14:42:57 +0200

    Updates

---
+++ precache-manifest.54872cb371bce5c174b3888847530826.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "420cd746c7f4814ba1cf",
+    "url": "/goit-react-hw-07-phonebook/static/css/main.ad14131f.chunk.css"
+  },
+  {
+    "revision": "420cd746c7f4814ba1cf",
+    "url": "/goit-react-hw-07-phonebook/static/js/main.f5c787af.chunk.js"
+  },
+  {
+    "revision": "d586fd842c3e6fc2d4e4",
+    "url": "/goit-react-hw-07-phonebook/static/js/runtime~main.b0c9dd7b.js"
+  },
+  {
+    "revision": "fd40eaa76573c5f0e5f5",
+    "url": "/goit-react-hw-07-phonebook/static/js/2.0bbdf0b5.chunk.js"
+  },
+  {
+    "revision": "4f091d501f8a6a820091e41282ef9a60",
+    "url": "/goit-react-hw-07-phonebook/index.html"
+  }
+];
